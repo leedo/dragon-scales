2a9ad3a48e1e2f3f671c45538f8c748b53d3b070
Lee Aylward <lee@laylward.com>
2012-12-28 13:18:00 -0500

    store rrds in id subdirectory

--- lib/Dragon/Scales.pm
+++ lib/Dragon/Scales.pm
@@ -7,7 +7,6 @@ use AnyEvent::Util;
 use AnyEvent::rrdcache;
 use Dragon::Scales::Worker;
 use Dragon::Scales::Request;
-use Data::Dump qw/pp/;
 
 sub new {
   my ($class, %args) = @_;
@@ -36,18 +35,21 @@ sub flush {
   my $time = AE::time;
   my $c = $self->{client};
 
-  for my $stat (keys %{$self->{buffer}}) {
-    my $value = delete $self->{buffer}{$stat};
-    my $file = "$self->{dir}/$stat.rrd";
+  for my $id (keys %{$self->{buffer}}) {
+    for my $stat (keys %{$self->{buffer}{$id}}) {
+      my $file = "$self->{dir}/$id/$stat.rrd";
+      my $value = $self->{$id}{$stat};
 
-    if (!-e $file) {
-      $self->create(split("-", $stat), sub {
-        $c->update($file, "$time:$value")
-      });
-      next;
+      if (!-e $file) {
+        $self->create($id, $stat, sub {
+          $c->update($file, "$time:$value")
+        });
+        next;
+      }
+
+      $c->update($file, "$time:$value", sub {});
     }
-
-    $c->update($file, "$time:$value", sub {});
+    delete $self->{buffer}{$id};
   }
 }
 
@@ -69,21 +71,23 @@ sub handle_req {
 sub incr {
   my ($self, $id, $req) = @_;
   my @stats = $req->parameters->get_all("stats");
-  $self->{buffer}{"$id-$_"}++ for @stats;
+  $self->{buffer}{$id}{$_}++ for @stats;
   $req->respond("ok");
 }
 
 sub create {
   my ($self, $id, $stat, $cb) = @_;
-  my $file = "$self->{dir}/$id-$stat.rrd";
+  my $file = "$self->{dir}/$id/$stat.rrd";
 
   die "file already exists" if -e $file;
+  mkdir "$self->{dir}/$id" unless -d "$self->{dir}/$id";
 
   rrd_create $file, {
       start => time,
       step  => 60,
     },
     "DS:$stat:GAUGE:120:U:U",
+    "RRA:AVERAGE:0.5:1:120", # one minute interval for 2 hours
     "RRA:AVERAGE:0.5:5:576", # five minute interval for two days
     "RRA:AVERAGE:0.5:30:1400", # thirty minute interval for a month
     "RRA:AVERAGE:0.5:720:704", # 12 hour interval for a year
@@ -93,17 +97,19 @@ sub create {
 sub fetch {
   my ($self, $id, $req) = @_;
   my $stat = $req->parameters->{stat};
-  my $file = "$self->{dir}/$id-$stat.rrd";
+  my $file = "$self->{dir}/$id/$stat.rrd";
+  my $time = time;
 
   rrd_fetch $file, {
-    start => time - 3600,
-    end   => time,
-    daemon => "unix:$self->{port}",
-  },
-  sub {
-    my ($samples, $err) = @_;
-    $req->respond([map { [$_->[0], $_->[1] || 0] } @$samples]);
-  };
+      start => $time - 3600,
+      end   => $time,
+      daemon => "unix:$self->{port}",
+    },
+    sub {
+      my $samples = shift;
+      return $req->error("unable to fetch") unless $samples;
+      $req->respond([map { [$_->[0], $_->[1] || 0] } @$samples]);
+    };
 }
 
 sub to_app {
